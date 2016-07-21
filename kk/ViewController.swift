c5cc7a5cffb3ef6867f5a89a674b95437fb4e23b
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-21 16:38:33 +0900

    Initial Commit

---
+++ kk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kk
+//
+//  Created by Seo Seonguk on 2016. 7. 21..
+//  Copyright © 2016년 BakeCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ kkTests/kkTests.swift
@@ -0,0 +1,36 @@
+//
+//  kkTests.swift
+//  kkTests
+//
+//  Created by Seo Seonguk on 2016. 7. 21..
+//  Copyright © 2016년 BakeCompany. All rights reserved.
+//
+
+import XCTest
+@testable import kk
+
+class kkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
